d8efd7c26c380ec21a37c1bdce89bb0c06d7b63b
kasra <kasramajdy81@gmail.com>
2025-02-12 00:35:48 +0330

    page error 40

--- src/app/pages/Contact/page.jsx
+++ src/app/pages/Contact/page.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div className='flex items-center justify-center py-[50px]'>
+      <Image src={"/img/404/404-error-page-with-explorer-man-illustration-on-white-background-vector.jpg"} width={500} height={500} alt='404' />
+    </div>
   )
 }
