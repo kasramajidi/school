8385802db2b08be34c5f5005b59655fe878db126
kasra <kasramajdy81@gmail.com>
2025-02-12 00:41:25 +0330

    fix page error 404

--- src/app/pages/Academics/page.jsx
+++ src/app/pages/Academics/page.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import Image from 'next/image'
 
 export default function page() {
   return (
-    <div className='flex items-center justify-center py-[50px]'>
-      <Image src={"/img/404/404-error-page-with-explorer-man-illustration-on-white-background-vector.jpg"} width={500} height={500} alt='404' />
-    </div>
+    <main>
+      kasra
+    </main>
   )
 }
